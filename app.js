a8ecab9b2d30b7deb4f2f9b75afa396d7255ecc3
aritra <aritrachowdhury65@gmail.com>
2022-02-13 20:21:00 +0530

    lossPercentage color changed

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var current = document.querySelector("#current");
 var check = document.querySelector(".check");
 var output = document.querySelector(".output");
 var error = document.querySelector(".error");
-var body = document.querySelector(".container");
 var clear = document.querySelector(".clear");
+var body = document.querySelector(".container");
 
 
 check.addEventListener("click", checkEventHandler);
@@ -18,15 +18,24 @@ function checkEventHandler(){
     if(Number(current.value) > Number(initial.value)){
       var profit = (Number(current.value) - Number(initial.value))*Number(quantity.value);
       var profitPercentage = (profit/ ((initial.value)* (quantity.value)))*100;
-      body.style.backgroundColor = "#16a34a"
+      
       output.style.color = "#16a34a"
       output.innerText = "Hurray !! Your Profit is Rs "+ profit.toFixed(2) + " and your Profit Percentage is "+ profitPercentage.toFixed(2) +"%";
       }else if(Number(initial.value) > Number(current.value)){
       var loss = (Number(initial.value) - Number(current.value))*Number(quantity.value);
       var lossPercentage = (loss / ((initial.value) * (quantity.value))) * 100;
-      body.style.backgroundColor = "#0284c7"
+      if(lossPercentage > 50){
+      body.style.backgroundColor = "grey"
       output.style.color = "#0284c7"
       output.innerText = "Ohh !! Your Loss is Rs "+ loss.toFixed(2)+" and your loss percentage is "+ lossPercentage.toFixed(2) +"%"
+      }
+      else {
+      body.style.backgroundColor = "#f0abfc"
+      output.style.color = "#0284c7"
+      output.innerText = "Ohh !! Your Loss is Rs "+ loss.toFixed(2)+" and your loss percentage is "+ lossPercentage.toFixed(2) +"%"
+      }
+
+      
       } else{
       output.innerText = "Ney !! Neither Profit nor Loss :)";
     }
